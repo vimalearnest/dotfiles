5315ac179d5d93a6e67493ff38d452bc401c88ae
Vimal Earnest <v_earnest@yahoo.com>
2019-09-26 14:34:12 -0700

    Added support for MELP package repo

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,5 +1,17 @@
+(require 'package)
+
+(add-to-list 'package-archives
+             '("MELPA Stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
 (add-hook 'term-mode-hook (lambda()
                             (setq yas-dont-activate t)))
 
 (setq dired-dwim-target t)
 (put 'dired-find-alternate-file 'disabled nil)
+
+(live-add-packs '("~/src/github/emacs-live-packs/haskell-pack"))
+(live-add-packs '("~/src/github/emacs-live-packs/erlang-pack"))
+(live-add-packs '("~/src/github/emacs-live-packs/elixir-pack"))
+(live-add-packs '("~/src/github/emacs-live-packs/projectile-pack"))
+(live-add-packs '("~/src/github/emacs-live-packs/rest-client-pack"))
